a04a0a335c5a93878a9fe101c1b9b3e96bd2e7df
kolya19951 <kolya.simotyuk@gmail.com>
2017-09-23 19:56:04 +0300

    task 3a

--- lab0.py
+++ lab0.py
@@ -1,26 +1,36 @@
 import csv
+import datetime
+
 # import pandas as pd
 
-# NYZHNYK, NAZARII, #11,
+# Symotiuk, Mykola, #13,
 
-k = 1                #порядковий номер другої літери імені (A = 1)
-n = (25 % 12) + 1      #залишок від ділення другої летери прізвища(Y = 25) на 12   +1
-y = 11 + 1996 #сума порядкового номеру у групі та числа 1996
+k = 25  # порядковий номер другої літери імені (A = 1)
+n = (25 % 12) + 1  # залишок від ділення другої летери прізвища(Y = 25) на 12   +1
+y = 13 + 1996  # сума порядкового номеру у групі та числа 1996
 m = 12 - n + 1
-z = 2015 - 11 #різниця 2015 - порядковий номер у групі
+z = 2015 - 13  # різниця 2015 - порядковий номер у групі
+l = []
 
 countRainyDays = 0
 
 with open('weather_madrid_LEMD_1997_2015.csv', 'r+') as myFile:
     reader = csv.reader(myFile)
-
-    for i in range(0, k + 4 + 1):    # +1 for adding header
-        line = myFile.readline()
-        print(line)
-
-
+    next(reader)
+    for line in myFile:
+        l.append(datetime.datetime.strptime(line[:line.find(',')], "%Y-%m-%d"))
+        if ((l[len(l) - 1].year == y) & (l[len(l) - 1].month == n) & (line.find('Rain') != -1)):
+            countRainyDays += 1
+            # for i in myFile:  # +1 for adding header
+            #     line = myFile.readline()
+            #     l.append(datetime.datetime.strptime(line[:line.find(',')], "%Y-%m-%d"))
+            #     print(line)
+            #     if ((l[len(l) - 1].year == y) & (l[len(l) - 1].month == n)):
+            #         print(l[len(l) - 1])
+            #         print(line + 'success')
 
 print(countRainyDays)
 
-
 myFile.close()
+
+# range(0, k + 4 + 1)
